4c34554feb90bd42a50ea3f5c7eddd2d49dfa7e3
GoodGuyForReal <tdoganay325@gmail.com>
2023-10-22 21:04:05 +0200

    resolver functions moved separate file

---
+++ resolvers.js
@@ -0,0 +1,31 @@
+import db from "./db.js";
+export const resolvers = {
+  Query: {
+    users: () => db.users,
+    blogs: () => db.blogs,
+    user: (_, args) => db.users.find((user) => user.id === args.id),
+    blog: (_, args) => db.blogs.find((blog) => blog.id === args.id),
+  },
+  User: {
+    blogs: (parent) => db.blogs.filter((r) => r.author_id === parent.id),
+  },
+  Blog: {
+    user: (parent) => db.users.find((r) => r.id === parent.author_id),
+  },
+  Mutation: {
+    deleteBlog: (_, args) => db.blogs.filter((r) => r.id !== args.id),
+    deleteUser: (_, args) => {
+      const selectedUser = db.users.find((r) => r.id === args.id);
+      if (!selectedUser) throw Error(`User with ID ${args.id} not found`);
+
+      const relatedBlogs = db.blogs.filter(
+        (r) => r.author_id === selectedUser.id
+      );
+      console.log("relatedBlogs", relatedBlogs);
+      const deletedUser = db.users.filter((r) => r.id !== args.id);
+      const deletedUserBlogs = db.blogs.filter((r) => r.author_id !== args.id);
+      console.log("deletedUserBlogs", deletedUserBlogs);
+      return deletedUser;
+    },
+  },
+};
