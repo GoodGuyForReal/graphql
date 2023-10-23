eec5ffe57babe433d2531f790c965de0c50d7197
GoodGuyForReal <tdoganay325@gmail.com>
2023-10-23 15:33:17 +0200

    TYPO FIXED

--- queries.js
+++ queries.js
@@ -58,7 +58,7 @@
 // Variables
 // {
 //     "blog": {
-//       "title": "New agen is good",
+//       "title": "New agent is good",
 //       "desc": "is gonna be new insta lock meta",
 //       "category": "Valorant",
 //       "author_id": "9c506193-f14b-469f-87fd-44267aa5a4d0",
